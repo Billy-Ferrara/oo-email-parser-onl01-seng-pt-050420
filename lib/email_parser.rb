6f1f0809ae7670c97b994d72cbf69fde37fa3cf5
Billy Ferrara <willfira717@gmail.com>
2020-05-31 18:53:07 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,14 +1,4 @@
 class EmailAddressParser
 
-  attr_accessor :emails
-
-  def initialize(emails)
-    @emails = emails
-  end
-
-  def parse
-    email_array = []
-    email_array 
-  end
-
+  
 end
